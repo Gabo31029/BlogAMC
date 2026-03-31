678f1366d53a41eecc5181bc971048d7965c71c1
Gabo31029 <gabrielchupaballesteros@gmail.com>
2026-03-31 10:51:13 -0500

    update 3

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .app.routes.auth import router as auth_router
-from .app.routes.posts import router as posts_router
-from .app.routes.uploads import router as uploads_router
+from app.routes.auth import router as auth_router
+from app.routes.posts import router as posts_router
+from app.routes.uploads import router as uploads_router
 
 
 app = FastAPI(title="BLOGAMC API", version="0.1.0")
